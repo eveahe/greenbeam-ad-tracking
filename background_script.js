c00bcc6568b51af8c379d7d6d9a3905e2f440150
eveahe <eve.ahearn@gmail.com>
2019-09-25 10:55:57 -0400

    Switched order of matching and overcame subdomain issue

--- background_script.js
+++ background_script.js
@@ -1,28 +1,37 @@
 // Put all the javascript code here, that you want to execute in background.
 let disconnectData;
+let firstRun = true;
+
+async function checkFirstRun() {
+    if (firstRun === true) {
+        loadDisconnectJson()
+    }
+}
+
+checkFirstRun()
+
+// import the parse domain module
+// const parseDomain = require("parse-domain");
 
 // function logURL(requestDetails) {
 //     if (requestDetails.url !== 'https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/entities.json') {
 //         console.log("Test Test logUrl: " + requestDetails.url)
+//         let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
+
+//         filter.ondata = event => {
+//             console.log(event.data);
+//         }
 //     }
 // }
 
-// browser.webRequest.onBeforeRequest.addListener(
+// browser.webRequest.onHeadersReceived.addListener(
 //     logURL, {
 //         urls: ["<all_urls>"]
-//     }
+//     },
+//     ["blocking", "responseHeaders"]
+
 // );
 
-var parseJson = function (response) {
-    return response.json()
-};
-
-fetch('https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/services.json')
-    .then(parseJson)
-    .then(data => {
-        var categoryName = Object.keys(data)[1]
-        console.log(data[categoryName])
-    })
 
 function loadDisconnectJson() {
     var parseJson = function (response) {
@@ -33,10 +42,10 @@ function loadDisconnectJson() {
         .then(parseJson)
         .then(data => {
             disconnectData = data;
+            return disconnectData;
         })
 }
 
-loadDisconnectJson()
 
 ////the below function is from:
 //https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
@@ -44,12 +53,12 @@ function extractHostName(url) {
     var hostname;
     //find & remove protocol (http, ftp, etc.) and get hostname
 
+    //remove the http://
     if (url.indexOf("//") > -1) {
         hostname = url.split('/')[2];
     } else {
         hostname = url.split('/')[0];
     }
-
     //find & remove port number
     hostname = hostname.split(':')[0];
     //find & remove "?"
@@ -59,42 +68,30 @@ function extractHostName(url) {
 }
 
 function checkUrlIsTracker(requestDetails) {
-    var urlHostName = extractHostName(requestDetails.url)
+    var urlHostName = extractHostName(requestDetails.url);
+    //giving our disconnect data list some time to load. 
+    if (disconnectData !== undefined) {
+        var jsonLength = Object.keys(disconnectData).length;
+        for (var i = 0; i < jsonLength; i++) {
+            var trackerName = Object.keys(disconnectData)[i];
+            var trackerUrlArray = disconnectData[trackerName].resources;
+            if (trackerUrlArray.some(function (v) {
+                    return urlHostName.indexOf(v) >=
+                        0;
+                })) {
+                console.log(`It seems that there is a match for ${urlHostName} in the sites run by the tracker ${trackerName}`)
+                let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
 
-    if (requestDetails.url !== 'https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/entities.json') {
-
-        var parseJson = function (response) {
-            return response.json()
-        };
-
-        fetch('https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/entities.json')
-            .then(parseJson)
-            .then(data => {
-                var jsonLength = Object.keys(data).length;
-                for (var i = 0; i < jsonLength; i++) {
-                    var trackerName = Object.keys(data)[i];
-                    var trackerUrlArray = data[trackerName].resources;
-                    if (trackerUrlArray.includes(urlHostName)) {
-                        console.log("hostname: " + urlHostName)
-                    }
-                    // console.log(trackerUrlArray.includes(urlHostName))
+                filter.ondata = event => {
+                    console.log(event.data);
                 }
-            })
-
+            }
+        }
     }
-    // var jsonLength = Object.keys(disconnectData).length;
-    // for (var i = 0; i < jsonLength; i++) {
-    //     var trackerName = Object.keys(disconnectData)[i]
-    //     var trackerUrlArray = disconnectData[trackerName].resources
-    //     console.log(trackerUrlArray)
-    //     console.log(trackerUrlArray.includes(requestDetails.url))
-
 }
 
-
-
-browser.webRequest.onBeforeRequest.addListener(
+browser.webRequest.onHeadersReceived.addListener(
     checkUrlIsTracker, {
         urls: ["<all_urls>"]
-    }
+    }, ["blocking", "responseHeaders"]
 );
